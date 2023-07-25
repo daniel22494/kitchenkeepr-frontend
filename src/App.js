7ed5c2376447e9529f12007bee755bb8f4a7161c
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-07-25 15:50:43 -0400

    display iframe testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { useState } from "react";
+
+const reel = "https://www.instagram.com/reel/CjxmT0oJkJH";
 
 function App() {
+  const [caption, setCaption] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="wrapper"
+        onClick={() => {
+          axios.get(`http://localhost:8080/?url=${reel}`).then(({ data }) => {
+            setCaption(data.caption);
+          });
+        }}
+      >
+        <div className="overlay"></div>
+        <iframe src={`${reel}/embed/captioned`}></iframe>
+      </div>
+      {caption && <pre>{caption}</pre>}
     </div>
   );
 }
