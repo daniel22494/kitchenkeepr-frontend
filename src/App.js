37726dd776c6bb2291ff7f0741745c0439926d3d
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-07-25 16:36:46 -0400

    created new components and app routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import axios from "axios";
+// import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { useState } from "react";
+import RecipeNav from "./components/RecipeNav/RecipeNav";
+// import { useState } from "react";
 
-const reel = "https://www.instagram.com/reel/CjxmT0oJkJH";
+// const reel = "https://www.instagram.com/reel/CjxmT0oJkJH";
 
 function App() {
-  const [caption, setCaption] = useState("");
   return (
-    <div className="App">
-      <div
-        className="wrapper"
-        onClick={() => {
-          axios.get(`http://localhost:8080/?url=${reel}`).then(({ data }) => {
-            setCaption(data.caption);
-          });
-        }}
-      >
-        <div className="overlay"></div>
-        <iframe src={`${reel}/embed/captioned`}></iframe>
-      </div>
-      {caption && <pre>{caption}</pre>}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RecipeNav />} />
+      </Routes>
+    </BrowserRouter>
   );
+  // const [caption, setCaption] = useState("");
+  // return (
+  //   <div className="App">
+  //     <div
+  //       className="wrapper"
+  //       onClick={() => {
+  //         axios.get(`http://localhost:8080/?url=${reel}`).then(({ data }) => {
+  //           setCaption(data.caption);
+  //         });
+  //       }}
+  //     >
+  //       <div className="overlay"></div>
+  //       <iframe src={`${reel}/embed/captioned`}></iframe>
+  //     </div>
+  //     {caption && <pre>{caption}</pre>}
+  //   </div>
+  // );
 }
 
 export default App;
